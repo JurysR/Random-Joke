6c1bfeda91c69bf61f9e203d6f8c18008a4a97c1
JurysR <jurys.r@gmail.com>
2021-07-10 17:07:31 +0200

    update app.js

--- app.js
+++ app.js
@@ -6,27 +6,9 @@ const URL = 'https://api.chucknorris.io/jokes/random';
 btn.addEventListener('click', () => {
   fetch(URL)
     .then((data) => data.json())
-    .then((response) => displayData(response));
+    .then((response) => displayData(response))
+    .catch((err) => console.log(err));
 });
-
-function getData(url) {
-  return new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState !== 4) return;
-      if (xhr.status === 200) {
-        resolve(xhr.responseText);
-      } else {
-        reject({
-          status: xhr.status,
-          text: xhr.statusText,
-        });
-      }
-    };
-  });
-}
 
 function displayData(data) {
   img.classList.add('shake-img');
